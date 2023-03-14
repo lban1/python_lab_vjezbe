36e974ad84fc7eaa5c5f77474397e2a8edc54f10
Luka Ban <lban1@tvz.hr>
2023-03-14 19:14:16 +0100

    Prve labaratorijske vjezbe; v1.0

---
+++ main.py
@@ -0,0 +1,24 @@
+#Ova linija mora biti komentar!
+
+print('Dobrodošli', '!'*3)
+
+# Napisati sljedeće izraze u jednoj liniji koda
+
+print('Unesite vaše ime: ', end='')
+ime = input()
+
+print('Unesite vaše prezime: ', end='')
+prezime = input()
+
+print('Unesite vaš JMBAG: ', end='')
+jmbg = input()
+
+# TODO zatražite od korisnika broj godina
+print('Unesite vaše godine: ', end='')
+godine = input()
+
+# TODO ime i prezime ispišite u navodnim znakovima
+
+print(f"Korisnik : ",'"', ime, " ", prezime, '"',  "", godine, end='!')
+
+print('Kraj')
